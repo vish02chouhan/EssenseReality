165e91642d3e8735a5d6554e8d747d498fc3f05e
harshabestway <harsha.chouhan@bestway.in>
2021-12-07 10:44:21 +0530

    added contact staff user to login user

--- EssenceRealty.Domain/Helper/StringExtension.cs
+++ EssenceRealty.Domain/Helper/StringExtension.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EssenceRealty.Scheduler.Helper
+namespace EssenceRealty.Domain.Helper
 {
     public static class StringExtensions
     {
