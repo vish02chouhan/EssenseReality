c5204fe76c2f3ac8deb6a30017eed25aa2cb659c
harshabestway <harsha.chouhan@bestway.in>
2021-12-07 10:23:41 +0530

    Added contact staff add and vcard download functionality

--- EssenceRealty.Domain/ViewModel/ContactStaffViewModel.cs
+++ EssenceRealty.Domain/ViewModel/ContactStaffViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Net;
 
 namespace EssenceRealty.Domain.ViewModels
 {
@@ -11,12 +14,9 @@ namespace EssenceRealty.Domain.ViewModels
         public string Role { get; set; }
         public bool? AdminAccess { get; set; }
         public string Position { get; set; }
-        public DateTime? LastLogin { get; set; }
         public string Email { get; set; }
         public string WebsiteUrl { get; set; }
         public string Username { get; set; }
-        public DateTime? Inserted { get; set; }
-        public DateTime? Modified { get; set; }
         public string Thumb_360PhotoURL { get; set; }
         public string OriginalPhotoURL { get; set; }
         public string About { get; set; }
@@ -25,6 +25,51 @@ namespace EssenceRealty.Domain.ViewModels
         public string InstagramProfile { get; set; }
         public string LinkedinProfile { get; set; }
         public ICollection<PhoneNumberViewModel> PhoneNumbers { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + LastName;
+        }
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("BEGIN:VCARD");
+            builder.AppendLine("VERSION:3.0");
+
+            // Name        
+            builder.Append("N:").Append(LastName)
+              .Append(";").AppendLine(FirstName);
+
+            // Full name        
+            builder.Append("FN:").Append(FirstName)
+              .Append(" ").AppendLine(LastName);
+
+            // Other data        
+            builder.Append("ORG:").AppendLine("Essence Realty");
+            builder.Append("TITLE:").AppendLine(Role);
+            builder.Append("TEL;TYPE=WORK:").AppendLine(PhoneNumbers.Where(x=>x.TypeCode == "W").Select(x=>x.Number).FirstOrDefault());
+            builder.Append("TEL;TYPE=HOME:").AppendLine(PhoneNumbers.Where(x => x.TypeCode == "H").Select(x => x.Number).FirstOrDefault());
+            builder.Append("TEL;TYPE=MOBILE:").AppendLine(PhoneNumbers.Where(x => x.TypeCode == "M").Select(x => x.Number).FirstOrDefault());
+            builder.Append("URL:").AppendLine(WebsiteUrl);
+            builder.Append("EMAIL;TYPE=EMAIL").AppendLine(Email);
+            builder.Append("socialProfile;TYPE=FACEBOOK:").AppendLine(FacebookProfile);
+            builder.Append("socialProfile;TYPE=TWITTER:").AppendLine();
+            builder.Append("socialProfile;TYPE=LINKEDIN:").AppendLine(LinkedinProfile);
+            builder.Append("socialProfile;TYPE=GPLUS:").AppendLine();
+            builder.Append("socialProfile;TYPE=INSTAGRAM:").AppendLine(InstagramProfile);
+            builder.Append("socialProfile;TYPE=YOUTUBE").AppendLine(Email);
+
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(OriginalPhotoURL);
+                builder.AppendLine("PHOTO;ENCODING=BASE64;TYPE=JPEG:");
+                builder.AppendLine(Convert.ToBase64String(data));
+                builder.AppendLine(string.Empty);
+            }
+
+            builder.AppendLine("END:VCARD");
+            return builder.ToString();
+        }
 
     }
 }
---
+++ EssenceRealty.Web.API/Controllers/VCardController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using EssenceRealty.Repository.IRepositories;
+using EssenceRealty.Domain.ViewModels;
+using EssenceRealty.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EssenceRealty.Web.API.Model;
+using Microsoft.AspNetCore.Authorization;
+using System.Text;
+
+namespace EssenceRealty.Web.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class VCardController : ControllerBase
+    {
+
+        private readonly ILogger<ContactStaffController> _logger;
+        private readonly IContactStaffRepository contactStaffRepository;
+        private readonly IMapper mapper;
+
+        public VCardController(ILogger<ContactStaffController> logger, IContactStaffRepository contactStaffRepository,
+            IMapper mapper)
+        {
+            _logger = logger;
+            this.contactStaffRepository = contactStaffRepository;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task Get(int id)
+        {
+            var result = await contactStaffRepository.GetByIdAsync(id);
+            var contactStaffViewModel = mapper.Map<ContactStaffViewModel>(result);
+            var fileName = contactStaffViewModel.GetFullName() + ".vcf";
+            var disposition = "attachment; filename=" + fileName;
+            var response = HttpContext.Response;
+
+            response.ContentType = "text/vcard";
+            response.Headers.Add("Content-disposition", disposition);
+            var bytes = Encoding.UTF8.GetBytes(contactStaffViewModel.ToString());
+            await response.Body.WriteAsync(bytes, 0, bytes.Length);
+
+            //return Ok(new EssenceResponse<ContactStaffViewModel>
+            //{
+            //    Data = contactStaffViewModel
+            //});
+        }
+    }
+  
+}
